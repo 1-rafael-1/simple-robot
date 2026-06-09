fe6f17a63be62be79aa7d76eb5344cbcfb5ec30b
1-rafael-1 <rafael.koch@gmx.net>
2026-06-09 15:36:51 +0200

    Change battery log level from info to debug

--- src/task/behavior/battery.rs
+++ src/task/behavior/battery.rs
@@ -1,12 +1,12 @@
 //! Battery-related behavior handlers.
 
-use defmt::info;
+use defmt::debug;
 
 use crate::{system::state::power, task::indicators::rgb_led_indicate};
 
 /// Handle battery measurement (level and voltage).
 pub async fn handle_battery_measured(level: u8, voltage: f32) {
-    info!("Battery level measured");
+    debug!("Battery level measured");
 
     {
         let mut state = power::POWER_STATE.lock().await;
